75ce49458183a19fd9688a0d9a4dbc96114e018e
Fay Vera <fvera.cavalcante@gmail.com>
2021-05-25 22:28:23 -0400

    creates user resources and adds controller actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :update, :destroy]
+
+    def create
+        @user = User.new(params)
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors
+        end        
+    end
+
+    def show
+        render json: @user
+    end
+
+    
+
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password_digest)
+    end
+end
+
+
