2b181a1e9ed138d4d70bc25bbe9721ecfdf78435
Fay Vera <fvera.cavalcante@gmail.com>
2021-05-25 23:50:56 -0400

    fixes password digest

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: "Kyle", email: "kyle@email.com", password_digest: 'password')
+User.create(name: "Kyle", email: "kyle@email.com", password: 'password')
