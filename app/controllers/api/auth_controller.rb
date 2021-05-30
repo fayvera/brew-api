381137a57834aee1ea194c8990315aee6f970fd2
Fay Vera <fvera.cavalcante@gmail.com>
2021-05-30 15:19:58 -0400

    adds jwt to user:create

---
+++ app/controllers/api/auth_controller.rb
@@ -0,0 +1,3 @@
+class Api::AuthController < ApplicationController
+    
+end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,12 +1,14 @@
 class Api::UsersController < ApplicationController
     before_action :set_user, only: [:show, :update, :destroy]
+    skip_before_action :authorize, only: [:create]
 
     def create
-        @user = User.new(params)
-        if @user.save
-            render json: @user
+        @user = User.create(user_params)
+        if @user.valid? 
+            @token = encode_token(user_id: @user.id)
+            render json: { user: @user, jwt: @token }, status: :created
         else
-            render json: @user.errors
+            render json: { error: 'failed to create user' }, status: :not_acceptable
         end        
     end
 
